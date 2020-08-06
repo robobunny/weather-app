73f1c472829ff0cdb73462489608859a1748c6fc
William Duraney <william.duraney@gmail.com>
2020-08-06 18:31:53 -0400

    add link to profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
 import Weather from './components/Weather';
+import Footer from './components/Footer';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <header>
+        <Header />
+      </header>
       <Weather />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (
+  <p>This app was created by <a href="https://robobunny.surge.sh">_robobunny</a></p>
+)
+
+export default Footer;
